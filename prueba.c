c5b8accdfb83454fe366d8f61dddf1c0588f355b
elhackerlibre <elhackerlibre@gmail.com>
2019-03-06 13:59:45 -0400

    mensaje

---
+++ prueba.c
@@ -0,0 +1,15 @@
+/*
+ * prueba.c
+ * Copyright (C) 2019 Felix Molero <elhackerlibre@gmail.com>
+ *
+ * Distributed under terms of the GPLv3+ license.
+ */
+
+#include "prueba.h"
+
+
+int main()
+{
+    printf("Hola Mundo");
+    return 0;
+}
